433a5f004ecd0c4f0c7f0c2cfaff7361c3c1e9e9
Paul <paul.p@itcgroup.io>
2025-01-02 15:41:15 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'keycloak-angular-demo-v2';
+}
