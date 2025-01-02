b8fabf17330130891fb307ed57743199fe8af302
Paul <paul.p@itcgroup.io>
2025-01-02 16:08:03 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { firstValueFrom, of } from 'rxjs';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    firstValueFrom(
+      of({
+        domain: 'abc.com',
+        realm: 'keycloak-angular-sandbox',
+        clientId: 'keycloak-angular',
+      })
+    ).then((config) => {
+      return keycloak.init({
+        config: {
+          realm: config.realm,
+          url: 'http://localhost:8080',
+          clientId: config.clientId,
+        },
+        initOptions: {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+            window.location.origin + '/assets/silent-check-sso.html',
+        },
+      });
+    });
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
